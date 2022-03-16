cc0b7c05285ad7ea04fc393f33d7a82a95bc2420
Georgia Shaw <georgia.shaw@digitaltheatre.com>
2022-03-16 20:31:52 -0300

    update page with new date

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [guess, setGuess] = useState("");
   return (
     <div className="grid">
       <div>
-        <input type="date" />
+        <div>{guess}</div>
+      </div>
+      <div>
+        <input
+          type="date"
+          id="input"
+          onChange={(event) => setGuess(event.target.value)}
+        />
         <button type="submit">Submit</button>
       </div>
     </div>
